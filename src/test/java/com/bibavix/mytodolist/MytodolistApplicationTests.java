108ea2e7ee1c8d82571a28bf687d324ea3dae80f
Jhorley666 <jhorley1998@gmail.com>
2025-05-26 19:32:52 -0500

    initial commmit

---
+++ src/test/java/com/bibavix/mytodolist/MytodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibavix.mytodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
